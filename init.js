c0cdf3a26cac2bf641eee6ca13749e630c6bb1e1
rohit verma <rohit729verma@gmail.com>
2026-03-27 13:31:40 +0530

    minor project on express node js and basic html css

---
+++ init.js
@@ -0,0 +1,32 @@
+// const mongoose = require("mongoose");
+// const Chat = require('./models/chat');
+
+// main().then(()=>{console.log('connection successful')})
+// .catch((err) => console.log(err));
+
+// async function main() {
+//   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+// }
+
+//  Chat.insertMany(
+//   [{
+//     from : 'ragini',
+//   to:'rohit',
+//   msg: 'i miss you ',
+//   created_at: new Date()
+//   },
+// {
+//     from : 'meeta',
+//   to:'rama',
+//   msg: 'i love you sister',
+//   created_at: new Date()
+//   },
+// {
+//     from : 'ashok',
+//   to:'aman',
+//   msg: 'provide the status of your work',
+//   created_at: new Date()
+//   }]
+// );
+
+// // chat1.save();
